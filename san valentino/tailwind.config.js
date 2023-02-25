c5e3716150c7ddfa017a687be0a871a87a16bcc3
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-02-25 17:26:17 +0100

    Add files via upload

---
+++ san valentino/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'hero-pattern':"url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu9teJmIFyC8ChV4nNaHs28jGjy10xabPWQr9BQtByiGyMOSXUa2X0BcES3yjdPYr7bG8&usqp=CAU')",
+        'footer-texture':"url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu9teJmIFyC8ChV4nNaHs28jGjy10xabPWQr9BQtByiGyMOSXUa2X0BcES3yjdPYr7bG8&usqp=CAU')",
+      }
+    },
+  },
+  plugins: [],
+}
